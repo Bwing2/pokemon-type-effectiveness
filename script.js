173c1f20ece7e579c4f6e04bb95538731c51ad23
Brandon Wing <brandon.wing245@gmail.com>
2024-01-08 14:36:56 -0700

    Switched to dropdown menu, added images and background.

--- script.js
+++ script.js
@@ -1,77 +1,136 @@
-const strongBtnEl = document.querySelector("#strongBtn");
-const weakBtnEl = document.querySelector("#weakBtn");
-const resultEl = document.querySelector("#result");
-const selectBoxEl = document.querySelector("#searchBox");
+const resultsBoxEl = document.querySelector('.resultsBox');
 
-strongBtnEl.addEventListener("click", () => {
+const strongBtnEl = document.querySelector('#strongBtn');
+const weakBtnEl = document.querySelector('#weakBtn');
+const textEl = document.querySelector('#text');
+const imagesEl = document.querySelector('#images');
+const selectBoxEl = document.querySelector('#selectBox');
+
+strongBtnEl.addEventListener('click', () => {
   strongAgainst();
 });
 
-weakBtnEl.addEventListener("click", () => {
+weakBtnEl.addEventListener('click', () => {
   weakAgainst();
 });
 
 // Array of all types
-const types = ["fire", "water", "grass"];
+const types = ['fire', 'water', 'grass'];
 
-// Creates a paragraph element and sets the text content equal to param "text" and text color equal to param "color"
-// Returns the created paragraph to be accessed outside function
+// Creates a paragraph element and sets the text content equal to param "text" and text color equal to param "color".
+// Returns the created paragraph to be accessed outside function.
 const createParagraph = (text, color) => {
-  const paragraph = document.createElement("p");
+  const paragraph = document.createElement('p');
   paragraph.innerHTML = text;
   paragraph.style.color = color;
   return paragraph;
 };
 
+// Creates and appends 2 images of types that a specific type is strong and weak against.
+const attachImages = (imageOnePath, imageTwoPath) => {
+  let imageOne = document.createElement('img');
+  // classList is added for styling purposes.
+  imageOne.classList.add('typeImages');
+  imageOne.src = imageOnePath;
+  // Inline-block styled so that they are on the same line.
+  imageOne.style.display = 'inline-block';
+  imageOne.style.height = '50px';
+  imageOne.style.width = '50px';
+
+  let imageTwo = document.createElement('img');
+  imageTwo.classList.add('typeImages');
+  imageTwo.src = imageTwoPath;
+  imageTwo.style.display = 'inline-block';
+  imageTwo.style.height = '50px';
+  imageTwo.style.width = '50px';
+
+  let results = document.querySelector('.resultsBox');
+  results.appendChild(imageOne);
+  results.appendChild(imageTwo);
+};
+
+//
 const strongAgainst = () => {
-  let lowerResult = selectBoxEl.value.toLowerCase();
+  // Gets value of selected option from dropdown menu.
+  let lowerResult = selectBoxEl.value;
 
-  // Still have to create new variable for text/color since they aren't defined in this function yet
+  // Gets rid of all text and images inside resultsBox div.
+  resultsBoxEl.innerHTML = '';
+
+  // Still have to create new variable for text/color since they aren't defined in this function yet.
   let text;
-  let color = "green";
+  let color = 'green';
 
-  if (lowerResult === "grass") {
-    text = "Strong against water.";
-  } else if (lowerResult === "fire") {
-    text = "Strong against grass.";
-  } else if (lowerResult === "water") {
-    text = "Strong against fire.";
+  if (lowerResult === 'grass') {
+    // Text shows up below images, because they are appended after since the attachImages() function runs inside the if statement.
+    text = 'Grass is strong against Water.';
+    attachImages(
+      './assets/images/grass_type_symbol.png',
+      './assets/images/water_type_symbol.png'
+    );
+  } else if (lowerResult === 'fire') {
+    text = 'Fire is strong against Grass.';
+    attachImages(
+      './assets/images/fire_type_symbol.png',
+      './assets/images/grass_type_symbol.png'
+    );
+  } else if (lowerResult === 'water') {
+    text = 'Water is strong against Fire.';
+    attachImages(
+      './assets/images/water_type_symbol.png',
+      './assets/images/fire_type_symbol.png'
+    );
   } else if (lowerResult !== types) {
-    text = "Please choose an existing type.";
-    color = "red";
-    // If type from the list isnt chosen, message is set to timeout in 3 seconds and be removed
+    text = 'Please choose an existing type.';
+    color = 'red';
+    // If type from the list isnt chosen, message is set to timeout in 3 seconds and be removed.
     setTimeout(() => {
-      resultEl.appendChild(messageBox).remove();
+      resultsBoxEl.innerHTML = '';
     }, 3000);
   }
 
-  // Create new variable that uses the createdParagraph() function to display the text/color related to searched word
-  const messageBox = createParagraph(text, color);
+  // Create new variable that uses the createdParagraph() function to display the text/color related to searched word.
+  // Has to run after if statements since text isn't defined yet.
+  const textBox = createParagraph(text, color);
 
-  // Sets the div to no text, and then appends the message
-  resultEl.innerHTML = "";
-  resultEl.appendChild(messageBox);
+  // Sets the div to no text, and then appends the text.
+  resultsBoxEl.appendChild(textBox);
 };
 
 const weakAgainst = () => {
-  let lowerResult = selectBoxEl.value.toLowerCase();
-  let message;
-  let color = "red";
+  let lowerResult = selectBoxEl.value;
 
-  if (lowerResult === "grass") {
-    message = "Weak to fire.";
-  } else if (lowerResult === "fire") {
-    message = "Weak to water.";
-  } else if (lowerResult === "water") {
-    message = "Weak to grass.";
+  resultsBoxEl.innerHTML = '';
+
+  let text;
+  let color = 'red';
+
+  if (lowerResult === 'grass') {
+    text = 'Grass is weak to Fire.';
+    attachImages(
+      './assets/images/fire_type_symbol.png',
+      './assets/images/grass_type_symbol.png'
+    );
+  } else if (lowerResult === 'fire') {
+    text = 'Fire is weak to Water.';
+    attachImages(
+      './assets/images/water_type_symbol.png',
+      './assets/images/fire_type_symbol.png'
+    );
+  } else if (lowerResult === 'water') {
+    text = 'Water is weak to Grass.';
+    attachImages(
+      './assets/images/grass_type_symbol.png',
+      './assets/images/water_type_symbol.png'
+    );
   } else if (lowerResult !== types) {
-    message = "Please choose an existing type.";
+    text = 'Please choose an existing type.';
     setTimeout(() => {
-      resultEl.appendChild(messageBox).remove();
+      resultsBoxEl.innerHTML = '';
     }, 3000);
   }
 
-  const messageBox = createParagraph(message, color);
-  resultEl.innerHTML = "";
-  resultEl.appendChild(messageBox);
+  const textBox = createParagraph(text, color);
+
+  resultsBoxEl.appendChild(textBox);
 };
